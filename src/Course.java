377e9ba540212995361a5d7f5dafb01c760f4dc6
dien180901 <dien180901@gmail.com>
2022-03-29 13:36:12 +0700

    Student and StudentEnrolment

---
+++ src/Course.java
@@ -0,0 +1,35 @@
+public class Course {
+    private String classId;
+    private String name;
+    private int numOfCredits;
+
+    public Course(String classId, String name, int numOfCredits) {
+        this.classId = classId;
+        this.name = name;
+        this.numOfCredits = numOfCredits;
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumOfCredits() {
+        return numOfCredits;
+    }
+
+    public void setNumOfCredits(int numOfCredits) {
+        this.numOfCredits = numOfCredits;
+    }
+}
