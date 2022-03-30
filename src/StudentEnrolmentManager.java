779b5e103c5534e9af2ce3e24144683a827b6e8b
dien180901 <dien180901@gmail.com>
2022-03-30 10:38:46 +0700

    StdentEnrolment

---
+++ src/StudentEnrolmentManager.java
@@ -0,0 +1,7 @@
+public interface StudentEnrolmentManager {
+    private void add(){};
+    private void update(){};
+    private void delete(){};
+    private void getOne(){};
+    private void getAll(){};
+}
